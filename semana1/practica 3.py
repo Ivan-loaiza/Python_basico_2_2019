88ee05a3f4161ee631ab44306d2fe359e8f18cbc
Ivan-loaiza <49097510+Ivan-loaiza@users.noreply.github.com>
2019-03-30 12:17:11 -0600

    Add files via upload

---
+++ semana1/practica 3.py
@@ -0,0 +1,15 @@
+#para revisar los tipos de datos
+
+#numero entero
+#definir un numero entero
+123456789
+mi_variable = 123456789
+#caso 1
+print('El valor de mi variable llamada "mi variable es"',mi_variable)
+
+#caso 2
+#cambio de comillas
+print("El valor de la variable llamda 'mi variable es'", mi_variable)
+#caso 3
+print("El valor de la variable llamada \"mi variable es\"", mi_variable)
+
---
+++ semana1/practica2.py
@@ -0,0 +1,11 @@
+#Esta es la practica par aescribir 2 lineas
+#En consola
+'''
+comentario de multiples líneas
+util para explicar mas detalles
+'''
+print('hello word!')
+print('good morning')
+#varias lineas en un solo print
+print('mim primera linea\nmim segunda linea')
+print('hola nuevo')
