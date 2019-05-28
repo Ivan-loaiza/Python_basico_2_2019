b92752bbefbc0d1428c011aceee59a12b18cc23b
ivan loaiza <49097510+Ivan-loaiza@users.noreply.github.com>
2019-05-28 13:09:58 -0600

    Add files via upload

---
+++ semana 7/Numpy3.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+x = np.array([0,1,2,3])
+
+
+# In[3]:
+
+
+x
+
+
+# In[4]:
+
+
+y = np.array([2,3,4,5])
+
+
+# In[5]:
+
+
+y
+
+
+# In[6]:
+
+
+x + 1
+
+
+# In[7]:
+
+
+x*x
+
+
+# In[8]:
+
+
+x * y
+
+
+# In[9]:
+
+
+x.sum()
+
+
+# In[10]:
+
+
+y.sum()
+
+
+# In[11]:
+
+
+y.T
+
+
+# In[ ]:
+
+
+
+
